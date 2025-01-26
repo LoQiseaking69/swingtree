65a942f232c20e826e9ccacf6085057790981528
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2025-01-26 13:43:53 -0500

    Create exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,7 @@
+class InvalidOperationError(Exception):
+    """Raised when an invalid operation is attempted on SwingTree."""
+    pass
+
+class IndexOutOfBoundsError(Exception):
+    """Raised when an index is out of valid range in SwingTree."""
+    pass
