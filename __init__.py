67a2a4209860b3588dc7906d97d0ea34da568df5
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2025-02-01 13:28:59 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,16 @@
 from .core import SwingTree, SwingTreeAPI
+from .exceptions import InvalidOperationError, IndexOutOfBoundsError, DataIntegrityError
+from .utils import is_power_of_two, next_power_of_two, validate_data
+from .coinbase_integration import CoinbaseTradingBot
 
-__all__ = ["SwingTree", "SwingTreeAPI"]
+__all__ = [
+    "SwingTree",
+    "SwingTreeAPI",
+    "InvalidOperationError",
+    "IndexOutOfBoundsError",
+    "DataIntegrityError",
+    "is_power_of_two",
+    "next_power_of_two",
+    "validate_data",
+    "CoinbaseTradingBot",
+]
